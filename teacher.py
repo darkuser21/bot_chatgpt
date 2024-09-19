2029d2dc95c7fe0fdfefe4c6632c15ac6f7b8bb1
Sanny Prajapati <101635131+darkuser21@users.noreply.github.com>
2024-09-19 20:54:03 +0530

    Update teacher.py

--- teacher.py
+++ teacher.py
@@ -21,7 +21,7 @@ if gptbutton:
         response = user.chat.completions.create(
             model = gptmodel,
             message = [
-                  {"role" : userrole, "content": pre_promt + prompt }
+                  {"role" : userrole, "content": pre_promt + promt }
             ]
         ) 
     me.snow()
