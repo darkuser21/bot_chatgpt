3301ae8cae4677e36947c9c11e08ad98cdd9563e
Sanny Prajapati <101635131+darkuser21@users.noreply.github.com>
2024-09-19 20:45:07 +0530

    Add files via upload

---
+++ teacher.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+import streamlit as me
+
+api = st.secrets["API_KEY"]
+user = OpenAI(api_key=api)
+gptmodel = "gpt-3.5-turbo"
+userrole = "user"
+
+pre_promt = "Teach me the following concept : "
+response = ""
+
+me.title("SASTA TEACHER")
+me.divider()
+promt=me.text_input("TUZE KYA SIKNA HAI JALDI BATA NAHI TO CHALA JAUNGA!! ")
+gptbutton = me.button("ARAM SE TOUCH KAR")
+me.caption("TABHI KAM KARUNGA JAB TU ESE PYAR SE TOUCH KAREGA")
+me.divider()
+
+if gptbutton:
+    with me.spinner(" SABAR KAR NAHI TO NIKAL"):
+        response = user.chat.completions.create(
+            model = gptmodel,
+            message = [
+                  {"role" : userrole, "content": pre_prompt + prompt }
+            ]
+        ) 
+    me.snow()
+    st.write(response.choice[0]. message.content)             
