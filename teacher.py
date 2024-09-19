bef99fabd32328b2a6b18662450d09866e9ccbe3
Sanny Prajapati <101635131+darkuser21@users.noreply.github.com>
2024-09-19 21:28:42 +0530

    Update teacher.py

--- teacher.py
+++ teacher.py
@@ -1,29 +1,31 @@
 from openai import OpenAI
 import streamlit as me
 
+# Get API key from secrets
 api = me.secrets["API_KEY"]
 user = OpenAI(api_key=api)
 gptmodel = "gpt-3.5-turbo-0125"
 userrole = "user"
 
-pre_promt = "Teach me the following concept : "
+pre_prompt = "Teach me the following concept: "
 response = ""
 
+# Streamlit interface
 me.title("SASTA TEACHER")
 me.divider()
-promt=me.text_input("TUZE KYA SIKNA HAI JALDI BATA NAHI TO CHALA JAUNGA!! ")
+prompt = me.text_input("TUZE KYA SIKHNA HAI JALDI BATA, NAHI TO CHALA JAUNGA!!")
 gptbutton = me.button("ARAM SE TOUCH KAR")
 me.caption("TABHI KAM KARUNGA JAB TU ESE PYAR SE TOUCH KAREGA")
 me.divider()
 
 if gptbutton:
-    with me.spinner(" SABAR KAR NAHI TO NIKAL"):
-        response = user.chat.completions.create
-        (
-            model = gptmodel,
-            messages = [
-                  {"role" : userrole, "content": pre_promt + promt }
+    with me.spinner("SABAR KAR NAHI TO NIKAL"):
+        response = user.chat_completions.create(
+            model=gptmodel,
+            messages=[
+                {"role": userrole, "content": pre_prompt + prompt}
             ]
-        ) 
+        )
+    
     me.snow()
-    me.write(response.choice[0]. messages.content)             
+    me.write(response.choices[0].message['content'])
